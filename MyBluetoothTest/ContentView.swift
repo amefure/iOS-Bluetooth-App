ebc189fd1c152160eca011b219977a4349caa865
amefure <shachi10969@gmail.com>
2024-01-04 18:32:25 +0900

    Initial Commit

---
+++ MyBluetoothTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBluetoothTest
+//
+//  Created by t&a on 2024/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBluetoothTest/MyBluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBluetoothTestApp.swift
+//  MyBluetoothTest
+//
+//  Created by t&a on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct MyBluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
